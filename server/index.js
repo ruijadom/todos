7d4d1f5fd9ffe97558b965bad6c3d6c71d74fb77
ruijadom <ruijadomingues@gmail.com>
2020-04-25 17:31:51 +0100

    routes to update and delete todos

--- server/index.js
+++ server/index.js
@@ -54,7 +54,32 @@ app.get("/todos/:id", async(req, res) => {
 
 // update a todo
 
+app.put("/todos/:id", async(req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2",
+    [description, id]);
+
+    res.json("Todo is updated");
+  } catch (err) {
+    console.log(err.message);
+    
+  }
+})
+
 // delete a todo
+
+app.delete("/todos/:id", async(req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+    res.json("Todo was deleted!")
+  } catch (err) {
+    console.log(err.message);
+    
+  }
+})
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
