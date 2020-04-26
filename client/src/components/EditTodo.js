2b52c39d3a399d47b1a24ce9f74cc3fc614200eb
ruijadom <ruijadomingues@gmail.com>
2020-04-26 12:44:35 +0100

    setDescription

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -34,14 +34,14 @@ const EditTodo = ({ todo }) => {
       </button>
 
 
-      <div className="modal" id={`id${todo.todo_id}`}>
+      <div className="modal" id={`id${todo.todo_id}`} onClick={() => setDescription(todo.description)}>
         <div className="modal-dialog">
           <div className="modal-content">
 
 
             <div className="modal-header">
               <h4 className="modal-title">Edit Todo</h4>
-              <button type="button" className="close" data-dismiss="modal">&times;</button>
+              <button type="button" className="close" data-dismiss="modal" onClick={() => setDescription(todo.description)}>&times;</button>
             </div>
 
 
@@ -61,7 +61,7 @@ const EditTodo = ({ todo }) => {
                 data-dismiss="modal"
                 onClick = {e => updateDescription(e)}
               >Save</button>
-              <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-danger" data-dismiss="modal" onClick={() => setDescription(todo.description)}>Close</button>
             </div>
 
           </div>
